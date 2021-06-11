73825b09ec4cf342e2e86543d9f4e4a848cec3a5
IsaPinzonV <isabellapv0305@outlook.com>
2021-06-11 16:12:33 -0500

    Cargar_imagenes

--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
+let bill, cinta, fuegoazul, fuegorosa, gnomo, portal, rosa, tienda, tiostan,
+  winner, raton, pag_grande, muercie, mabel, hacha, gameO, dipper, diario_gran, alfombra, abuela, corazon,
+  abuelo, cuarto, escogerP, hombreTauro, bosque, paginas, inicial, tronco;
+
+function preload() {
+  bill = loadImage("/assets/0bill0.png");
+  cinta = loadImage("/assets/0cinta0.png");
+  fuegoazul = loadImage("/assets/0fuegoazul0.png");
+  fuegorosa = loadImage("/assets/0fuegorosa0.png");
+  gnomo = loadImage("/assets/0gnomo0.png");
+  portal = loadImage("/assets/0portal0.png");
+  rosa = loadImage("/assets/0rosa0.png");
+  tienda = loadImage("/assets/0tienda0.png");
+  tiostan = loadImage("/assets/0tiostan0.png");
+  winner = loadImage("/assets/winner.png");
+  raton = loadImage("/assets/raton.png");
+  pag_grande = loadImage("/assets/pag_grande.png");
+  muercie = loadImage("/assets/muercie.png");
+  mabel = loadImage("/assets/mabel.png");
+  hacha = loadImage("/assets/hacha.png");
+  gameO = loadImage("/assets/gameO.png");
+  dipper = loadImage("/assets/dipper.png");
+  diario_gran = loadImage("/assets/diario_gran.png");
+  alfombra = loadImage("/assets/alfombra.png");
+  abuela = loadImage("/assets/Abuela Fantasma.png");
+  corazon = loadImage("/assets/CorazónVida.png");
+  abuelo = loadImage("/assets/ABUELO FANTASMA.png");
+  cuarto = loadImage("/assets/Cuarto Dipper_2-8.png");
+  escogerP = loadImage("/assets/EscogerPersonaje-8.png");
+  hombreTauro = loadImage("/assets/HombreTauro.png");
+  bosque = loadImage("/assets/Mapa Bosque-8.png");
+  paginas = loadImage("/assets/PaginasDelLibro-8.png");
+  inicial = loadImage("/assets/Pantalla inicial-8.png");
+  tronco = loadImage("/assets/Tronco.png");
+}
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 500);
 }
 
 function draw() {
-  background(220);
+  //background(220);
+  image(inicial,0,0);
 }
