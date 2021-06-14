caf43157bf5110515eae0fbce254b60645b5fd61
Alejo902 <alejomurcia902@hotmail.com>
2021-06-14 08:29:27 -0500

    Cambios Y arreglar

--- AbuelaF.js
+++ AbuelaF.js
@@ -25,24 +25,6 @@ class AbuelaF extends Enemigo {
     }
   }
 
-  mostrar() {
-    image(abuela, this.x, this.y);
-  }
-
-  verificarImpacto() {
-    // además de marcarlo como muerto... retorna el valor para usarlo
-    if (
-      this.x > personaje.x &&
-      this.x < personaje.x + 78 &&
-      this.y > personaje.y &&
-      this.y < personaje.y + 117
-    ) {
-      this.muerto = true; // muere cuando lo tocan no?
-      return true;
-    }
-    return false;
-  }
-
   estaMuerto() {
     return this.muerto;
   }
